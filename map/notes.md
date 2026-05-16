e97adab1cfcf76b2337af9ca7f7cf5cba33291f9
Peirui Liu <lpr2333@sjtu.edu.cn>
2026-05-16 16:02:21 +0800

    完成了不含插入、iterator、删除的红黑树查找逻辑、架构设计等等

---
+++ map/notes.md
@@ -0,0 +1,87 @@
+# Map
+
+## map的本质
+
+实现一个 key -> value 的有序映射
+
+底层实现通常是红黑树，按照key排序
+
+每个节点存 pair<const Key, T> 
+
+树满足
+- BST 顺序
+- 红黑树平衡约束
+- 中序遍历得到有序的 key
+
+## 这题和前三个容器不同
+
+| 容器               | 核心难点                              |
+| ---------------- | --------------------------------- |
+| `vector`         | 连续内存、扩容、生命周期                      |
+| `priority_queue` | 树形结构、合并、异常安全                      |
+| `deque`          | 分块坐标、双端维护、iterator                |
+| `map`            | **平衡 BST、旋转修复、按序 iterator、erase** |
+
+难点
+
+- 红黑树插入修复
+- 红黑树删除修复
+- iterator 的中序前驱 / 后继
+- erase 时候的节点替换和 iterator语义
+- operator[] 、 at 、 insert 的细节
+
+## 采用标准CLRS 红黑树
+**经典红黑树+父指针**
+
+每个节点维护
+- parent
+- left 
+- right
+- color 0为红，1为黑，2为双黑（删除时临时状态）
+- value
+
+## 节点设计的关键问题
+
+**有些测试没有默认构造函数**
+
+所以不能设计出Node() 然后让里面的 value 进行默认构造，那会爆炸
+
+节点只有在真正插入的时候再用pair构造
+
+先考虑用nullptr表示叶子，不用带value的NIL哨兵
+
+
+## iterator 的思路，比deque更 "树味"
+
+map 的 iterator 不再是下标，而是 owner + current_node
+++ it 找中序后继
+    有右子树就找右子树最左
+    没有右子树就沿着 parent 上去，直到当前节点是父节点的左子树
+-- it 找中序前驱
+    有左子树就找左子树最右
+    没有左子树就沿着 parent 上去，直到当前节点是父节点的右子树
+
+
+## end() 怎么表示
+
+- A: 用nullptr
+- B: 用一个特殊的哨兵节点,但是会引入哨兵节点构造问题
+
+所以用nullptr 的话、然后用 ++max_node = nullptr \ --end() = 最大节点
+
+
+## 怎么析构？
+
+Node里面有一个pair<const Key, T> value
+所以析构的时候要调用 value 的析构函数
+value.~pair() 就行了
+
+
+## 设计Node
+
+value有拷贝构造函数吗？
+插入的时候直接用 pair<const Key, T>(key, value) 构造就行了，不需要先构造一个空的再赋值
+
+value有复制赋值函数吗？
+
+我如果只有key,我怎么插入一个节点？
--- map/src/map.hpp
+++ map/src/map.hpp
@@ -24,6 +24,57 @@ template<
    * You can use sjtu::map as value_type by typedef.
    */
   typedef pair<const Key, T> value_type;
+
+  /*节点类，需要我们维护pa*/
+  class Node
+  {
+  public:
+    Node* parent;
+    Node* left;
+    Node* right;
+    size_t color; /* 0 red|1 black|2 double black*/
+    value_type value;
+
+    /*默认构造函数不存在*/
+    /*至少传入一个value*/
+    Node(value_type val):value(val){
+      parent = nullptr;
+      left = nullptr;
+      right = nullptr;
+      color = 0; /*默认为红节点*/
+    }
+    /*有跌的构造函数*/
+    Node(value_type val, Node* papa):value(val){
+      parent = papa;
+      left = nullptr;
+      right = nullptr;
+      color = 0;
+    }
+    /*析构*/
+    ~Node(){
+      parent = nullptr;
+      left = nullptr;
+      right = nullptr;
+      color = 0;
+      value.~pair();
+    }
+
+    /*Node的复制构造函数*/
+    Node(Node* other){
+      /*Node的复制我们直接一个Node复制整颗树出来,用于map里面的构造*/
+      if(other == nullptr) return;
+      else{
+        this->color = other->color;
+        this->value = other->value;
+        this->left = new Node*(other->left);
+        this->right = new Node*(other->right);
+        this->parent = new Node*(other->parent);
+      }
+    }
+  };
+
+
+
   /**
    * see BidirectionalIterator at CppReference for help.
    *
@@ -112,22 +163,57 @@ template<
     // And other methods in iterator.
   };
 
+  /*
+  *map的参数
+  *
+  */
+  Node* root_node;
+  Compare cmp;
+  size_t current_size;
   /**
    * TODO two constructors
    */
-  map() {}
+  map() {
+    root_node = new Node*();
+    current_size = 0;
+  }
 
-  map(const map &other) {}
+  map(const map &other) {
+    /*直接用内置的超级爆爆爆NOOOOOOOOOOOOODEEEEEEEEEEEEEEEEE构造函数整个给你杀了*/
+    this->root_node = new Node(other.root_node);
+    this->cmp = other.cmp;
+    this->current_size = other.current_size;
+  }
 
   /**
    * TODO assignment operator
    */
-  map &operator=(const map &other) {}
+  map &operator=(const map &other) {
+    if(this == &other) return *this;
+    else{
+      /*删除自己*/
+      delete_node(this->root_node);
+      /*开始复制*/
+      this->root_node = new Node(other.root_node);
+      this->cmp = other.cmp;
+      this->current_size = other.current_size;
+    }
+  }
 
+  /*辅助函数，删除Node的函数*/
+  void delete_node(Node* node){
+    if(node == nullptr) return;
+    delete_node(node->left);
+    delete_node(node->right);
+    node->~Node();
+  }
   /**
    * TODO Destructors
    */
-  ~map() {}
+  ~map() {
+    /*从根节点一个一个调用析构函数*/
+    delete_node(this->root_node);
+  }
 
   /**
    * TODO
@@ -135,9 +221,37 @@ template<
    * Returns a reference to the mapped value of the element with key equivalent to key.
    * If no such element exists, an exception of type `index_out_of_bound'
    */
-  T &at(const Key &key) {}
+  T &at(const Key &key) {
+    /*BST tree 的 查找*/
+    Node* search_node = root_node;
+    while(search_node && !cmp(search_node->value.first,key) && !cmp(key,search_node->value.first)){
+      if(cmp(search_node->value.first , key )) search_node = search_node->right;
+      else{
+        search_node = search_node->left;
+      }
+    }
+    if(search_node == nullptr){
+      throw index_out_of_bound();
+    }else{
+      return search_node->value.second;
+    }
+  }
 
-  const T &at(const Key &key) const {}
+  const T &at(const Key &key) const {
+    /*BST tree 的 查找*/
+    Node* search_node = root_node;
+    while(search_node && !cmp(search_node->value.first,key) && !cmp(key,search_node->value.first)){
+      if(cmp(search_node->value.first , key )) search_node = search_node->right;
+      else{
+        search_node = search_node->left;
+      }
+    }
+    if(search_node == nullptr){
+      throw index_out_of_bound();
+    }else{
+      return search_node->value.second;
+    }
+  }
 
   /**
    * TODO
@@ -145,12 +259,29 @@ template<
    * Returns a reference to the value that is mapped to a key equivalent to key,
    *   performing an insertion if such key does not already exist.
    */
-  T &operator[](const Key &key) {}
+  T &operator[](const Key &key) {
+    Node* search_node = root_node;
+    /*这个地方涉及到插入,我们先不要处理*/
+  }
 
   /**
    * behave like at() throw index_out_of_bound if such key does not exist.
    */
-  const T &operator[](const Key &key) const {}
+  const T &operator[](const Key &key) const {
+    /*BST tree 的 查找*/
+    Node* search_node = root_node;
+    while(search_node && !cmp(search_node->value.first,key) && !cmp(key,search_node->value.first)){
+      if(cmp(search_node->value.first , key )) search_node = search_node->right;
+      else{
+        search_node = search_node->left;
+      }
+    }
+    if(search_node == nullptr){
+      throw index_out_of_bound();
+    }else{
+      return search_node->value.second;
+    }
+  }
 
   /**
    * return a iterator to the beginning
@@ -171,17 +302,28 @@ template<
    * checks whether the container is empty
    * return true if empty, otherwise false.
    */
-  bool empty() const {}
+  bool empty() const {
+    return this->current_size == 0;
+  }
 
   /**
    * returns the number of elements.
    */
-  size_t size() const {}
+  size_t size() const {
+    return this->current_size;
+  }
 
   /**
    * clears the contents
    */
-  void clear() {}
+  void clear() {
+    delete_node(this->root_node->left);
+    delete_node(this->root_node->right);
+    this->root_node->value.~pair();
+    this->current_size = 0 ;
+    this->root_node->left = nullptr;
+    this->root_node->right = nullptr;
+  }
 
   /**
    * insert an element.
@@ -205,7 +347,17 @@ template<
    *     since this container does not allow duplicates.
    * The default method of check the equivalence is !(a < b || b > a)
    */
-  size_t count(const Key &key) const {}
+  size_t count(const Key &key) const {
+    Node* search_node = root_node;
+    while(search_node != nullptr && !cmp(search_node->value.key , key) && !cmp(key , search_node->value.key)){
+      if(cmp(search_node->value.first , key )) search_node = search_node->right;
+      else{
+        search_node = search_node->left;
+      }
+    }
+    if(search_node == nullptr) return 0;
+    return 1;
+  }
 
   /**
    * Finds an element with key equivalent to key.
